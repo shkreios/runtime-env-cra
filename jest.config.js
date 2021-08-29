d2bd7a08f844d2e7b0a33de5c4b5d5e9d6a52f0f
Simon <simon.hessel@kreios.lu>
2021-08-29 23:28:03 +0200

    Adapt jest for typescript

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,16 @@ const { defaults } = require('jest-config');
 
 module.exports = {
   ...defaults,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   // set verbose to true if you dont want to see logs
-  testMatch: ['<rootDir>/tests/**/*.spec.js'],
+  testMatch: ['<rootDir>/tests/**/*.spec.ts'],
   verbose: true,
   restoreMocks: true,
   modulePathIgnorePatterns: ['./examples'],
   testPathIgnorePatterns: ['./node_modules'],
   collectCoverage: true,
-  collectCoverageFrom: ['lib/**/{!(index),}.js'],
+  collectCoverageFrom: ['src/**/{!(index),}.ts'],
   coverageDirectory: '<rootDir>/coverage-report',
   reporters: ['default', 'jest-junit'],
   coverageThreshold: {
